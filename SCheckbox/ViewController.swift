9bae9c441baaddb4f67e886cc7c5d38a3c15708f
= <ganaya@ia.com.mx>
2014-09-25 18:10:19 -0500

    Initial Commit

---
+++ SCheckbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SCheckbox
+//
+//  Created by Guillermo Anaya Magallón on 25/09/14.
+//  Copyright (c) 2014 wanaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
